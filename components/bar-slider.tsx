dfe1b6f6d4d473ed9f0adc031b3069bc2cf28673
Josua Stuck <jstuck@ethz.ch>
2023-06-04 01:09:34 +0200

    dynamically change barplots

--- components/bar-slider.tsx
+++ components/bar-slider.tsx
@@ -16,17 +16,18 @@ export default function BarSlider(props: BarSliderProps) {
   const { width = 600, height = 400, padding = 20 } = props
 
   const svgRef = useRef(null)
+  const [data, setData] = useState<DataEntry[]>([])
 
   const [selectedYear, setSelectedYear] = useState(2021)
 
   useEffect(() => {
-    // Set up the SVG container
-    const data: DataEntry[] = getInterpolatedDataEntries(selectedYear)
+    setData(getInterpolatedDataEntries(selectedYear))
+  }, [selectedYear])
+
+  useEffect(() => {
 
     // Set up the SVG container
     const svg = d3.select(svgRef.current)
-
-    svg.selectAll('*').remove()
 
     // Set up scales
     const xScale = d3.scaleLinear()
@@ -39,85 +40,102 @@ export default function BarSlider(props: BarSliderProps) {
       .range([0, height])
       .padding(0.2)
 
+    // Bind data
+    const cars = svg.selectAll('.bar.cars').data(data);
+    const bikes = svg.selectAll('.bar.bikes').data(data);
 
-    svg.selectAll('.bar.cars')
-      .data(data)
-      .enter()
+    // Update selection for cars
+    cars
+      .transition()
+      .duration(500)
+      .attr('x', (d: any) => width / 2 - xScale(Math.abs(d.cars)))
+      .attr('width', (d: any) => xScale(Math.abs(d.cars)));
+
+    // Enter selection for cars
+    cars.enter()
       .append('rect')
       .attr('class', 'bar cars')
-      .attr('x', d => width / 2 - xScale(Math.abs(d.cars)))
       .attr('y', (d: DataEntry) => yScale(d.knr)!)
-      .attr('width', d => xScale(Math.abs(d.cars)))
       .attr('height', yScale.bandwidth())
       .style('fill', colorPalette.c1)
+      .transition()
+      .duration(500)
+      .attr('x', (d: any) => width / 2 - xScale(Math.abs(d.cars)))
+      .attr('width', (d: any) => xScale(Math.abs(d.cars)));
 
+    // Exit selection for cars
+    cars.exit()
+      .transition()
+      .duration(500)
+      .attr('width', 0)
+      .remove();
 
+    // Update selection for bikes
+    bikes
+      .transition()
+      .duration(500)
+      .attr('x', width / 2)
+      .attr('width', (d: any) => xScale(Math.abs(d.bikes)));
 
-    svg.selectAll('.bar.bikes')
-      .data(data)
-      .enter()
+    // Enter selection for bikes
+    bikes.enter()
       .append('rect')
       .attr('class', 'bar bikes')
-      .attr('x', width / 2)
       .attr('y', (d: DataEntry) => yScale(d.knr)!)
-      .attr('width', (d:any) => xScale(Math.abs(d.bikes)))
       .attr('height', yScale.bandwidth())
       .style('fill', colorPalette.c9)
-
-    // Apply transition to update bar lengths
-    svg.selectAll('.bar.cars')
+      .transition()
+      .duration(500)
       .attr('x', width / 2)
+      .attr('width', (d: any) => xScale(Math.abs(d.bikes)));
+
+    // Exit selection for bikes
+    bikes.exit()
+      .transition()
+      .duration(500)
       .attr('width', 0)
-      .transition()
-      .duration(500) // Transition duration
-      .attr('x', (d: any) => (d.cars < 0 ? width / 2 : width / 2 - xScale(Math.abs(d.cars))))
-      .attr('width', (d: any) => xScale(Math.abs(d.cars)))
+      .remove();
 
-    svg.selectAll('.bar.bikes')
-      .attr('x', width / 2)
-      .attr('width', 0)
-      .transition()
-      .duration(500) // Transition duration
-      .attr('x', (d: any) => (d.bikes < 0 ? width / 2 - xScale(Math.abs(d.bikes)) : width / 2))
-      .attr('width', (d: any) => xScale(Math.abs(d.bikes)))
-
-
-
-    svg.selectAll('.bar.bikes')
-      .transition()
-      .duration(500) // Transition duration
-      .attr('x', width / 2)
-      .attr('width', (d: any) => xScale(Math.abs(d.bikes)))
-
-    svg.append('line')
+    const line = svg.selectAll<SVGLineElement, null>('.center-line').data([null]);
+    line.enter()
+      .append('line')
+      .attr('class', 'center-line')
+      .merge(line)
       .attr('x1', width / 2)
       .attr('y1', 0)
       .attr('x2', width / 2)
       .attr('y2', height)
       .style('stroke', 'black')
-      .style('stroke-width', 1)
+      .style('stroke-width', 1);
 
+    // Adjust scale for x-axis
     xScale.range([0, width])
-    const xAxis = d3.axisBottom(xScale).ticks(9)
-    const yAxis = d3.axisLeft(yScale)
+    const xAxis = d3.axisBottom(xScale).ticks(9);
 
-    svg.append('g')
+    // Update x-axis if it exists, or append a new one if it doesn't
+    const xAxisGroup = svg.selectAll<SVGGElement, null>('.x-axis').data([null]);
+    xAxisGroup.enter()
+      .append('g')
+      .attr('class', 'x-axis')
       .attr('transform', `translate(0, ${height})`)
+      .merge(xAxisGroup)
       .transition()
       .duration(500)
       .call(xAxis)
       .selectAll('text')
-      .style('font-size', '12px')
+      .style('font-size', '12px');
 
+    // Y-axis remains constant, no need for update pattern
+    const yAxis = d3.axisLeft(yScale);
     svg.append('g')
       .transition()
       .duration(500)
       .call(yAxis)
       .selectAll('text')
-      .style('font-size', '12px')
+      .style('font-size', '12px');
 
-    return () => {}
-  }, [svgRef, height, width, selectedYear])
+    return () => { }
+  }, [svgRef, height, width, data])
 
 
   // TODO: take input from range and update map accordingly.
