17a5db569bd3fd297bbac7c7141f5ea67925ebb0
Silas Meier <silas.meier@bluewin.ch>
2023-06-03 13:59:47 +0200

    Added second visualization

--- pages/index.tsx
+++ pages/index.tsx
@@ -350,8 +350,150 @@ function Slider() {
           </div>
           <div id="year-slider">
             <input type="range" min="2015" max="2023" defaultValue={selectedYear} step="1"
-                   onChange={e => setSelectedYear(e.target.value)}></input>
+                   onChange={e => setSelectedYear(e.target.value.toString())}></input>
           </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+function SnapshotInTime() {
+
+  const svgRef = useRef(null)
+
+  const [width, setWidth] = useState(600)
+  const [height, setHeight] = useState(400)
+
+  const padding = 20;
+
+  const [selectedYear, setSelectedYear] = useState("2023")
+
+  /* Sample data, replace with correct one. */
+  // TODO: Fetch data from API
+  interface FilteredData {
+    knr: string;
+    cars: number;
+    bikes: number;
+  }
+
+  const dataAll: any = {
+    "2023": [
+      {knr: "1", cars: 1500, bikes: 1900},
+      {knr: "2", cars: 1600, bikes: 1250},
+      {knr: "3", cars: 1700, bikes: 1300},
+      {knr: "4", cars: 1800, bikes: 1350},
+      {knr: "5", cars: 1500, bikes: 1400},
+      {knr: "6", cars: 1950, bikes: 1250},
+      {knr: "7", cars: 1700, bikes: 1500},
+      {knr: "8", cars: 1790, bikes: 1900},
+      {knr: "9", cars: 2000, bikes: 1750},
+      {knr: "10", cars: 1790, bikes: 1900},
+      {knr: "11", cars: 1790, bikes: 1900},
+      {knr: "12", cars: 1790, bikes: 1900},
+    ], "2022": [
+      {knr: "1", cars: 1300, bikes: 3200},
+      {knr: "2", cars: 1500, bikes: 1550},
+      {knr: "3", cars: 1100, bikes: 1300},
+      {knr: "4", cars: 1800, bikes: 350},
+      {knr: "5", cars: 1100, bikes: 400},
+      {knr: "6", cars: 1950, bikes: 1250},
+      {knr: "7", cars: 1300, bikes: 1500},
+      {knr: "8", cars: 1190, bikes: 900},
+      {knr: "9", cars: 2000, bikes: 1750}
+    ],
+  };
+
+  useEffect(() => {
+    const svgD3 = d3.select(svgRef.current)
+    svgD3
+      .attr('preserveAspectRatio', 'xMinYMin meet')
+      .attr('viewBox', `${-padding} 0 ${width + 2 * padding} ${height + 2 * padding}`)
+  }, [svgRef, width, height]);
+
+  useEffect(() => {
+    const data: FilteredData[] = dataAll[selectedYear];
+
+    // Set up the SVG container
+    const svg = d3.select(svgRef.current).append('g');
+
+    // Set up scales
+    const xScale = d3.scaleLinear()
+      .domain([-d3.max(data, d => d.cars)!,
+                       d3.max(data, d => d.cars)!])
+      .range([0, width/2]);
+
+    const yScale = d3.scaleBand()
+      .domain(data.map(d => d.knr))
+      .range([0, height])
+      .padding(0.2);
+
+    // Render bars for males
+    svg.selectAll(".bar.cars")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("x", d => width / 2 - xScale(Math.abs(d.cars)))
+      .attr("y", (d: FilteredData) => yScale(d.knr)!)
+      .attr("width", d => xScale(Math.abs(d.cars)))
+      .attr("height", yScale.bandwidth())
+      .style("fill", colorPalette.c1);
+
+    // Render bars for females
+    svg.selectAll(".bar.bikes")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("x", width / 2)
+      .attr("y", (d: FilteredData) => yScale(d.knr)!)
+      .attr("width", d => xScale(Math.abs(d.bikes)))
+      .attr("height", yScale.bandwidth())
+      .style("fill", colorPalette.c9);
+
+    // Draw the black line
+    svg.append("line")
+      .attr("x1", width / 2)
+      .attr("y1", 0)
+      .attr("x2", width / 2)
+      .attr("y2", height)
+      .style("stroke", "black")
+      .style("stroke-width", 1);
+
+    // Add axes
+    // Adjust the xScale range to fit the new width
+    xScale.range([0, width]);
+    const xAxis = d3.axisBottom(xScale).ticks(9);
+    const yAxis = d3.axisLeft(yScale);
+
+    svg.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(xAxis)
+      .selectAll("text")
+      .style("font-size", "12px");
+
+    svg.append("g")
+      .call(yAxis)
+      .selectAll("text")
+      .style("font-size", "12px");
+
+    return () => {
+      svg.remove()
+    }
+
+  }, [svgRef, height, width])
+
+  // TODO: take input from range and update map accordingly.
+  return (
+    <div>
+      <div className="slider">
+        <div className="graph w-full">
+          <svg ref={svgRef} className="slider-svg w-full">
+          </svg>
+        </div>
+        <div id="year-slider">
+          <input type="range" min="2015" max="2023" defaultValue={selectedYear} step="1"
+                 onChange={e => setSelectedYear(e.target.value)}></input>
         </div>
       </div>
     </div>
@@ -390,6 +532,8 @@ export default function Home() {
         </div>
         <div className="fade-in home-visualization w-full md:w-4/6 lg:w-3/6">
           <Slider />
+          <hr />
+          <SnapshotInTime />
         </div>
 
       </div>
